ce3296f464b4f87c584ac1b6826264c3f2de9767
Anatoli Surmach <ansur1608@gmail.com>
2018-06-26 18:32:25 +0300

    1-module 2-task and 3-task are done.

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,10 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let regexp = /-?\d+(\.\d+)?/g;
 
+    let result = str.match(regexp);
+    let arr = result.map(Number);
+    return { min: Math.min( ...arr ), max: Math.max( ...arr ) };
 }
 
