72038e86b6cd5147f8903aff5bd8578e666bcb70
Anatoli Surmach <ansur1608@gmail.com>
2018-06-29 22:34:58 +0300

    module-2 tasks are done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,15 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+    let clonedObj = {};
+
+    for (let key in obj){
+        if(obj[key] === null || typeof(obj[key]) !== 'object')
+            clonedObj[key] = obj[key];
+        else
+            clonedObj[key] = clone(obj[key]);
+    }
+
+    return clonedObj;
 }
