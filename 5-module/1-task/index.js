7786924eea44b29964bcbb60ac38182860a77801
Anatoli Surmach <ansur1608@gmail.com>
2018-07-08 15:39:17 +0300

    5-module task is done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,27 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let rows = table.lastElementChild.getElementsByTagName('tr');
+
+    for (let row of rows) {
+        let avail = row.children[3].dataset.available,
+            gender = row.children[2].innerHTML,
+            age = +row.children[1].innerHTML;
+
+        if (!avail) row.setAttribute('hidden', '');
+
+        if (avail === 'true') {
+            row.classList.add('available');
+        } else {
+            row.classList.add('unavailable');
+        }
+
+        if (gender === 'm') {
+            row.classList.add('male');
+        } else {
+            row.classList.add('female');
+        }
+
+        if (age < 18) row.style.textDecoration = 'line-through';
+    }
 }
